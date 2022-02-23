da40f08e2f3a4523174c8675bbb59b4820013b1d
kiwity <88898878+KaioLl90@users.noreply.github.com>
2022-02-24 01:47:07 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Owner = [Kiwity](https://t.me/TT54T)
+
+# Support = [Group](https://t.me/YU9XL)
+
+-----
+- [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
---
+++ translation.py
@@ -0,0 +1,20 @@
+class Translation(object):
+    START_TEXT = """-بوت استخراج تطبيقات المطور @TT54T
+شكرا الاستخدام للبوت
+يرجي ارسل رقم هاتفك, للحصول علي APP-ID from my.telegram.org
+/start لاعاده تعين البينات"""
+    AFTER_RECVD_CODE_TEXT = """
+ الان لقد وصل الكود اليك قم بأرساله الينا كتابه
+ فقط لاستخدام فقط للحصول علي APP ID from my.telegram.org
+لمعرفه كيفيه الاستخدام تواصل مع المطور
+/start لاعداه كتابه البينات"""
+
+    BEFORE_SUCC_LOGIN = "رمز التحقق. لاستخراج ..."
+
+    ERRED_PAGE = "حدث خطأا. فشل في الحصول علي API ID. \n\n@TT54T"
+
+    CANCELLED_MESG = "سلام يرجي ارسال /start للاستخدام مرا اخري"
+
+    IN_VALID_CODE_PVDED = "عذرا, يوجد خطأ غير معروف"
+
+    IN_VALID_PHNO_PVDED = "عذرا, عذرا يوجد خطا في رقم الهاتف"
